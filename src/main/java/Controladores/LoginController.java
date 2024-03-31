5efebdb8c404724e2b4abbb87869f6ff2e36860a
Alph4Bet4 <aperbla737@g.educaand.es>
2024-03-31 15:48:05 +0200

    Creacion de Proyecto para el Cliente

---
+++ src/main/java/Controladores/LoginController.java
@@ -0,0 +1,34 @@
+package Controladores;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button btnInicioSesion;
+
+    @FXML
+    private Hyperlink hyperLink;
+
+    @FXML
+    private PasswordField lblContrasenia;
+
+    @FXML
+    private TextField lblCorreoElectronico;
+
+    @FXML
+    void iniciarSesion(ActionEvent event) {
+
+    }
+
+    @FXML
+    void registrarseHyperLink(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,18 @@
+module org.tfg_apb.tfg_apb_cliente {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens org.tfg_apb.tfg_apb_cliente to javafx.fxml;
+    exports org.tfg_apb.tfg_apb_cliente;
+    exports Controladores;
+    opens Controladores to javafx.fxml;
+}
