56f27efaca1e5ab0e2a6c27321b11439b79cdf4a
Alph4Bet4 <aperbla737@g.educaand.es>
2024-05-12 21:53:50 +0200

    Agregado el Jar.

---
+++ src/main/java/org/tfg_apb/tfg_apb_cliente/Main_1.java
@@ -0,0 +1,9 @@
+package org.tfg_apb.tfg_apb_cliente;
+
+import java.io.IOException;
+
+public class Main_1 {
+    public static void main(String[] args) throws IOException {
+        Main.main(args);
+    }
+}
